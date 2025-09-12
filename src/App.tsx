81686585176f6bee978d422ab08ee2ce6662114d
andreydedey <dedey.cardoso@gmail.com>
2025-09-12 15:32:31 -0300

    fix: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <div className="flex min-h-svh flex-col items-center justify-center">
-      asdfglkj
+      <Button variant={"default"}>Click me</Button>
     </div>
   );
 }
